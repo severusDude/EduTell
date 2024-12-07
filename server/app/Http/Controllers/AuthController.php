a2661b396294b84911833f4b0d22bab2d668dc78
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2024-12-07 16:48:57 +0700

    feat(auth): add register user

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+
+    public function register()
+    {
+        $request = request(['name', 'email', 'password']);
+
+        User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password'])
+        ]);
+
+        return response()->json(['message' => 'Account created successfully'], 200);
+    }
 
     /**
      * Get a JWT via given credentials.
--- server/routes/api.php
+++ server/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('api')->prefix('auth')
     ->controller(AuthController::class)
     ->group(function () {
+        Route::post('/register', 'register');
         Route::post('/login', 'login');
         Route::post('/logout', 'logout');
         Route::post('/refresh', 'refresh');
