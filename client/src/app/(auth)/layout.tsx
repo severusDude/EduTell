27040813052354efdbab21f9c41d8aead2f95b84
raihannurhidayat <grandrx041@gmail.com>
2024-12-08 15:30:55 +0700

    feat: add view auth & responsif

---
+++ client/src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default LoginLayout;
