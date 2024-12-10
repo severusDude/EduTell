9e949455d3cf787cc35862564222a1fc1493ce2b
severusDude <fajarsaladin05@gmail.com>
2024-12-11 01:03:05 +0700

    feat(subchapter): add subchapter model

---
+++ server/app/Http/Controllers/SubchapterController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subchapter;
+use Illuminate\Http\Request;
+
+class SubchapterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Subchapter $subchapter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Subchapter $subchapter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Subchapter $subchapter)
+    {
+        //
+    }
+}
