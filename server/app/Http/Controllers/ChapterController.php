5f250a1ea920f2b217ad3477c1daf2d321621fe8
severusDude <fajarsaladin05@gmail.com>
2024-12-10 20:19:58 +0700

    feat(chapter): add chapter model

---
+++ server/app/Http/Controllers/ChapterController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chapter;
+use Illuminate\Http\Request;
+
+class ChapterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chapter $chapter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chapter $chapter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chapter $chapter)
+    {
+        //
+    }
+}
