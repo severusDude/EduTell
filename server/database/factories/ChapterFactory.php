d0fbaf29715cb8dfec2809befeffb8a0f92a9c50
severusDude <fajarsaladin05@gmail.com>
2024-12-10 21:15:03 +0700

    chore(chapter): add chapter seeder

--- server/database/factories/ChapterFactory.php
+++ server/database/factories/ChapterFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class ChapterFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(6),
+            'description' => fake()->text(),
+            'is_published' => true,
+            'course_id' => Course::pluck('id')->random(),
         ];
     }
 }
