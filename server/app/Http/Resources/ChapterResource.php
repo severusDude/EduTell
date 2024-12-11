f18a4bb295207a392b78ccb9466ff3f498fb7211
severusDude <fajarsaladin05@gmail.com>
2024-12-11 14:39:58 +0700

    feat(chapter): remove id from resource

--- server/app/Http/Resources/ChapterResource.php
+++ server/app/Http/Resources/ChapterResource.php
@@ -15,7 +15,6 @@ class ChapterResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'position' => $this->position,
