def641cafe35376939f11d192d403a7c85e3a154
severusDude <fajarsaladin05@gmail.com>
2024-12-13 11:26:08 +0700

    feat(submission): set up submission model

--- server/app/Models/Submission.php
+++ server/app/Models/Submission.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Submission extends Model
 {
-    //
+    public $incrementing = false;
+
+    protected $fillable = [
+        'content'
+    ];
+
+    public static function booted(): void
+    {
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
 }
