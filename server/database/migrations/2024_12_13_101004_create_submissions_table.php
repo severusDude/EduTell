f8aac374a86cce4563a17cd1585227958905341f
severusDude <fajarsaladin05@gmail.com>
2024-12-13 11:11:09 +0700

    feat(submission): create submission table

--- server/database/migrations/2024_12_13_101004_create_submissions_table.php
+++ server/database/migrations/2024_12_13_101004_create_submissions_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('submissions', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained();
+            $table->foreignUuid('assignment_id')->constrained();
+            $table->text('content')->nullable();
             $table->timestamps();
         });
     }
