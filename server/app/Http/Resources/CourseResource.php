58dd1cc71ab0fd1cd6825c872ed9e3348da75765
severusDude <fajarsaladin05@gmail.com>
2024-12-14 22:51:24 +0700

    fix(course): casts purchased to string

--- server/app/Http/Resources/CourseResource.php
+++ server/app/Http/Resources/CourseResource.php
@@ -33,7 +33,7 @@ class CourseResource extends JsonResource
             'enrolled' => $this->students()->count(),
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
-            'purchased' => ($user && $user->hasPurchased($course)) ?
+            'purchased' => (string) ($user && $user->hasPurchased($course)) ?
                 $course->students()->where('user_id', $user->id)->first()->pivot->purchased_at ??
                 $course->teacher->slug // for teacher
                 : null,
